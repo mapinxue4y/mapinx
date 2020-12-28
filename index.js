5b8d569eb4af19e108d0599d305456b4b0e1927d
mapinxue4y <73047992+mapinxue4y@users.noreply.github.com>
2020-12-28 13:56:02 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('hello world!');
+});
+
+app.listen(8000,()=>{
+    console.log('run on 8000');
+});
